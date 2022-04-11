2333126d9215c1b4a1926427bf4f84f5237373cc
Yan Zingra <yan.zingra12@gmail.com>
2022-04-11 00:10:34 -0300

    Spring-initializer with docker-compsoe

---
+++ src/test/java/yan/trainning/saleservice/SaleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package yan.trainning.saleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
